6875f50664b85acded142265fe648b5e31ade309
Stewart666 <132806667+Stewart666@users.noreply.github.com>
2023-10-21 01:13:25 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function history() {
+    let paragraph = "Our company was created on 29/09/2001 and started to work on 01/10/2001. Created by Ibrahim Mbapila Photographer, Erick Chengelela Finance manager and Stewart Milton Website developer together they created this company"
+    document.getElementById("p").innerHTML=paragraph;
+    document.getElementById("picture1").src="https://img.freepik.com/premium-photo/new-photo-camera-black-background-closeup_151013-32324.jpg?size=626&ext=jpg&ga=GA1.1.266876037.1696102206&semt=ais";
+    document.getElementById("picture2").src="https://img.freepik.com/free-photo/ai-generated-3d-water_23-2150684739.jpg?size=626&ext=jpg&ga=GA1.1.266876037.1696102206&semt=ais";
+}
+function contacts(){
+    let ErickContacts = "+255-621259484";
+    let IbrahimContacts = "+255-686809841";
+    let IbrahimEmail = "nxusajames@gmail.com";
+    let StewartContacts = "+255 710244313";
+    let StewartEmail = "stewartmilton666@gmail.com";
+    alert("Call Erick Chengelela Finance manager on " + ErickContacts + " Thank your your")
+    alert("Call Ibrahim Mbapila Photographer on " + IbrahimContacts + " or send a message on " + IbrahimEmail + " Thank your your")
+    alert("Call Stewart Milton Website developer on " + StewartContacts + " or send a message on " + StewartEmail + " Thank your your")
+}
+function thanks(){
+    let thanks = "Thank you for your job odder 🙏🙏🙏 we promise you we will do a good job with you"
+    alert(thanks)
+}
